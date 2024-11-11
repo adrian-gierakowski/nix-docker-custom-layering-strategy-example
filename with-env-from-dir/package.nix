781b5e2184a5c8a1e2e67dc79c569adf4b6e6581
Adrian Gierakowski <agierakowski@gmail.com>
2024-11-11 19:15:44 +0000

    use makeShellApplication and fix issuse in with-env-from-dir

--- with-env-from-dir/package.nix
+++ with-env-from-dir/package.nix
@@ -9,22 +9,25 @@ let
   env = "${pkgs.coreutils}/bin/env";
   grep = "${pkgs.gnugrep}/bin/grep";
 in
-pkgs.writers.writeBash "with-env-from-dir" ''
-  set -ueo pipefail
+pkgs.writeShellApplication {
+  name = "with-env-from-dir";
+  text = ''
+    set -ueo pipefail
 
-  env_dir=$1
-  shift
+    env_dir=$1
+    shift
 
-  for env_var_file_path in $env_dir/*; do
-    env_var_name=$(${basename} $env_var_file_path)
+    for env_var_file_path in "$env_dir"/*; do
+      env_var_name=$(${basename} "$env_var_file_path")
 
-    # Only export if variable is not already defined.
-    if [ -z "$(${env} | ${grep} "^''${env_var_name}=")" ]
-    then
-      env_var_value=$(${cat} $env_var_file_path)
-      export $env_var_name="$env_var_value"
-    fi
-  done
+      # Only export if variable is not already defined.
+      if ! ${env} | ${grep} -q "^''${env_var_name}="
+      then
+        env_var_value=$(${cat} "$env_var_file_path")
+        export "$env_var_name"="$env_var_value"
+      fi
+    done
 
-  exec ''${@}
-''
+    exec "''${@}"
+  '';
+}
